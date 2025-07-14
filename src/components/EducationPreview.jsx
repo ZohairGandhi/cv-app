ca61c1ceb9211d2aea4112a070d479b31c38a4f0
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-07-14 17:04:03 +1000

    fix: hide section heading in preview if no data for that section

--- src/components/EducationPreview.jsx
+++ src/components/EducationPreview.jsx
@@ -2,28 +2,30 @@ import "../styles/EduExpPreview.css";
 
 function EducationPreview({ educationList }) {
   return (
-    <div className="education-preview">
-      <p className="sect-head">EDUCATION</p>
-      <hr />
+    educationList.length > 0 && (
+      <div className="education-preview">
+        <p className="sect-head">EDUCATION</p>
+        <hr />
 
-      {educationList.map((item) => {
-        return (
-          <div className="edu-item">
-            <div>
-              <p className="institution">{item.institution}</p>
-              <p>{item.location}</p>
-            </div>
+        {educationList.map((item) => {
+          return (
+            <div className="edu-item">
+              <div>
+                <p className="institution">{item.institution}</p>
+                <p>{item.location}</p>
+              </div>
 
-            <div>
-              <p>{item.degree}</p>
-              <p>
-                {item.start} - {item.grad}
-              </p>
+              <div>
+                <p>{item.degree}</p>
+                <p>
+                  {item.start} - {item.grad}
+                </p>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    )
   );
 }
 
